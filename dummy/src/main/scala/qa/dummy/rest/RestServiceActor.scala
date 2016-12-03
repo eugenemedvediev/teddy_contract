30751b4f86a6939a5e4baa7028f1f2c354a28b21
ievgen <eugene.medvediev@gmail.com>
2016-12-03 22:01:27 +0100

    root html page and json others

--- dummy/src/main/scala/qa/dummy/rest/RestServiceActor.scala
+++ dummy/src/main/scala/qa/dummy/rest/RestServiceActor.scala
@@ -10,8 +10,6 @@ import org.json4s.jackson.Serialization
 import qa.common.model.Configuration
 import qa.dummy.DummyCreator
 import qa.dummy.db.DB
-import spray.http.ContentTypes._
-import spray.http.HttpHeaders._
 import spray.http._
 import spray.httpx.unmarshalling._
 import spray.routing._
@@ -21,6 +19,7 @@ import spray.routing._
   */
 class RestServiceActor(val db: DB, val servers: scala.collection.mutable.Map[String, APIServer]) extends Actor with RestService {
   implicit def actorRefFactory = context
+
   def receive = runRoute(rest)
 }
 
@@ -40,95 +39,64 @@ trait RestService extends HttpService with SLF4JLogging {
   private val toPort = sys.env.getOrElse(toPortKey, toPortValue).toInt
   val contracts = "contracts"
 
-  protected val rest = respondWithMediaType(MediaTypes.`application/json`) {
+  protected val rest =
     path("") {
-      respondWithHeader(`Content-Type`(`application/json`)) {
-        complete(
-          StatusCodes.OK,
-          """
-            |<!DOCTYPE html>
-            |<html>
-            | <head>
-            |   <title>Contract Servers</title>
-            | </head>
-            | <body>
-            |   <h1>%s</h1>
-            |   <h3>API:<h3>
-            |   <ul>
-            |     <li>
-            |       path: /contracts
-            |       method: GET
-            |       description: Get all contracts
-            |     </li>
-            |<li>
-            |path: /contracts/{port}
-            |method: DELETE
-            |description: Delete specified contract
-            |</li>
-            |<li></li>
-            |</ul>
-            |<h3>Configuration:</h3>
-            |<pre><code>%s</code></pre>
-            |
-            |</body>
-            |</html>
-          """.stripMargin
-        )
-      }
+      getFromResource("index.html")
     } ~
-      path(contracts) {
-      post {
-        entity(Unmarshaller(MediaTypes.`application/json`) {
-          case httpEntity: HttpEntity => {
-            val json: JValue = parse(httpEntity.asString(HttpCharsets.`UTF-8`))
-            implicit val formats = DefaultFormats
-            json.extract[Configuration]
-          }
-        }) {
-          configuration: Configuration =>
+      respondWithMediaType(MediaTypes.`application/json`) {
+        path(contracts) {
+          post {
+            entity(Unmarshaller(MediaTypes.`application/json`) {
+              case httpEntity: HttpEntity => {
+                val json: JValue = parse(httpEntity.asString(HttpCharsets.`UTF-8`))
+                implicit val formats = DefaultFormats
+                json.extract[Configuration]
+              }
+            }) {
+              configuration: Configuration =>
+                ctx: RequestContext => {
+                  val server = DummyCreator.createServer(fromPort, configuration)
+                  server.start
+                  val port = server.getPort
+                  if (port > toPort) {
+                    server.stop()
+                    ctx.complete(StatusCodes.BadRequest, s"""{"error": "Port limit reached; $fromPort-$toPort"}""")
+                  } else {
+                    servers.put(port.toString, server)
+                    db.writeConfiguration(port.toString, configuration)
+                    ctx.complete(StatusCodes.OK, s"""{"port": $port}""")
+                  }
+                }
+            }
+          } ~ get {
             ctx: RequestContext => {
-              val server = DummyCreator.createServer(fromPort, configuration)
-              server.start
-              val port = server.getPort
-              if (port > toPort) {
-                server.stop()
-                ctx.complete(StatusCodes.BadRequest, s"""{"error": "Port limit reached; $fromPort-$toPort"}""")
-              } else {
-                servers.put(port.toString, server)
-                db.writeConfiguration(port.toString, configuration)
-                ctx.complete(StatusCodes.OK, s"""{"port": $port}""")
+              val configurations = db.getAllStartedConfigurations.map(x => (x._1.toString, x._2))
+              implicit val formats = DefaultFormats
+              ctx.complete(StatusCodes.OK, Serialization.write(configurations))
+            }
+          }
+        } ~
+          path(contracts / IntNumber) { port =>
+            delete {
+              ctx: RequestContext => {
+                try {
+                  val server = servers(port.toString)
+                  servers.remove(port.toString)
+                  server.stop()
+                } catch {
+                  case e: Exception => log.error(s"Attempt to delete not existing server: ${e.getMessage}")
+                }
+
+                try {
+                  db.deleteConfiguration(port.toString)
+                } catch {
+                  case e: Exception => log.error(s"Attempt to delete not existing server from db ${e.getMessage}")
+                }
+
+                ctx.complete(StatusCodes.NoContent)
               }
             }
-        }
-      } ~ get {
-        ctx: RequestContext => {
-          val configurations = db.getAllStartedConfigurations.map(x => (x._1.toString, x._2))
-          implicit val formats = DefaultFormats
-          ctx.complete(StatusCodes.OK, Serialization.write(configurations))
-        }
-      }
-    } ~
-      path(contracts / IntNumber) { port =>
-        delete {
-          ctx: RequestContext => {
-            try {
-              val server = servers(port.toString)
-              servers.remove(port.toString)
-              server.stop()
-            } catch {
-              case e: Exception => log.error(s"Attempt to delete not existing server: ${e.getMessage}")
-            }
-
-            try {
-              db.deleteConfiguration(port.toString)
-            } catch {
-              case e: Exception => log.error(s"Attempt to delete not existing server from db ${e.getMessage}")
-            }
-
-            ctx.complete(StatusCodes.NoContent)
           }
-        }
       }
-  }
 
 }
